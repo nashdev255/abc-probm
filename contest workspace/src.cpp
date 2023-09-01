6fa6c4e10f926638a30a0fe554546dea5747a8c2
nashdev255 <yutori1107@gmail.com>
2023-09-01 17:43:32 +0900

    ignore-test

--- contest workspace/src.cpp
+++ contest workspace/src.cpp
@@ -2,9 +2,6 @@
 using namespace std;
 
 int main() {
-    int a, b;
-    cin >> a >> b;
-    if((a*b)%2) cout << "Odd" << endl;
-    else cout << "Even" << endl;
+    cout << "Hello World" << endl;
     return 0;
 }
