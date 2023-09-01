e89b8cc85771cf63862cbd41900c19c73d3fd514
nashdev255 <yutori1107@gmail.com>
2023-09-01 18:36:58 +0900

    add abc087/b.cpp

---
+++ abc087/b.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b, c, x;
+    cin >> a >> b >> c >> x;
+    int totalWays = 0;
+    for(int i=0;i<=a;i++) {
+        for(int j=0;j<=b;j++) {
+            for(int k=0;k<=c;k++) {
+                if(500*i+100*j+50*k==x) totalWays++;
+            }
+        }
+    }
+    cout << totalWays << endl;
+    return 0;
+}
