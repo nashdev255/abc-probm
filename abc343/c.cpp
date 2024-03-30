5d5ed810877c55d1d741e7b23b9347635b520de2
nashdev255 <nashdev255@gmail.com>
2024-03-31 01:15:03 +0900

    Add abc343-c

--- abc343/c.cpp
+++ abc343/c.cpp
@@ -1,9 +1,17 @@
 #include <bits/stdc++.h>
-
-using std::cin;
-using std::cout;
-using std::endl;
+#define rep(i,n) for(int i=0;i<n;++i)
+using namespace std;
+using ll = long long;
 
 int main() {
+  ll n,ans;
+  cin>>n;
+  for(ll i=0;i*i*i<=n;i++){
+    string str=to_string(i*i*i);
+    string rstr=to_string(i*i*i);
+    reverse(rstr.begin(),rstr.end());
+    if(str==rstr)ans=i*i*i;
+  }
+  cout<<ans<<endl;
   return 0;
 }
